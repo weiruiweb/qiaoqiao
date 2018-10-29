646d1a54be8f5617238471a3f55f85c3e4fe103d
weiruiweb <18192654258@163.com>
2018-10-29 11:23:30 +0800

    coomit

---
+++ pages/order_get/order_get.js
@@ -0,0 +1,45 @@
+import {Api} from '../../utils/api.js';
+var api = new Api();
+const app = getApp();
+import {Token} from '../../utils/token.js';
+const token = new Token();
+
+Page({
+  data: {
+    is_show:false,
+    currentId:0,
+  },
+  //事件处理函数
+  preventTouchMove:function(e) {
+
+  },
+  mask:function(e){
+   this.setData({
+    is_show:false,
+   })
+ },
+ show:function(e){
+  this.setData({
+    is_show:true,
+   })
+ },
+ 
+  intoPath(e){
+    const self = this;
+    api.pathTo(api.getDataSet(e,'path'),'nav');
+  },
+
+  intoPathRedi(e){
+    const self = this;
+    wx.navigateBack({
+      delta:1
+    })
+  },
+  intoPathRedirect(e){
+    const self = this;
+    api.pathTo(api.getDataSet(e,'path'),'redi');
+  }, 
+ 
+})
+
+  
