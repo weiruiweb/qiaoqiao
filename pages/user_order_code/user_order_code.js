d99f79c1a624fca8d3f8f19e21e082d1b63a4fb6
Mrzhzhzh <877854886@qq.com>
2019-01-31 17:56:43 +0800

    test

--- pages/user_order_code/user_order_code.js
+++ pages/user_order_code/user_order_code.js
@@ -55,11 +55,11 @@ Page({
   	const self = this;
   	if(self.data.sForm.passage2){  
       self.data.searchItem.passage2 = self.data.sForm.passage2;
-      self.getMainData(true,self.data.sForm.passage2)
+      self.getMainData(true)
       
     }else if(self.data.sForm.passage2==''){
       if(wx.getStorageSync('threeInfo').user_type==1){
-        api.showToast('请输入取货码  ','none');
+        api.showToast('请输入取货码','none');
         return
       };
       if(wx.getStorageSync('threeInfo').user_type==2){
@@ -72,7 +72,7 @@ Page({
 
 
 
-  getMainData(isNew,Name){
+  getMainData(isNew){
     const self = this;
     if(isNew){
       api.clearPageIndex(self)
@@ -174,7 +174,7 @@ Page({
   },
 
 
-  orderUpdateTwo(e){
+    orderUpdateTwo(e){
     const self = this;
     var index = api.getDataSet(e,'index');
     console.log(index)
@@ -194,25 +194,42 @@ Page({
       id:self.data.id,
       user_type:0
     };
+    wx.showModal({
+       title: '确认部分核销',
+       content: '是否部分核销此订单',
+       showCancel: true,//是否显示取消按钮
+       cancelText:"取消",//默认是“取消”
+       cancelColor:'#ff0000',//取消文字的颜色
+       confirmText:"确定",//默认是“确定”
+       confirmColor: '#ff0000',//确定文字的颜色
+       success: function (res) {
+          if (res.cancel) {
+             //点击取消,默认隐藏弹框
+          } else {
+            const callback  = (res)=>{
+		      if(res.solely_code==100000){  
+		        api.showToast('已添加备注','none')
+		      }else{
+		        api.showToast(res.msg,'none')
+		      }
+		      self.data.mainData = [];
+		      self.data.sForm.title='';
+		      self.setData({
+		        web_sForm:self.data.sForm,
+		      });
+		       
+		      if(wx.getStorageSync('threeInfo').user_type==2){
+		        self.getMainData(true) 
+		      };
 
-    const callback  = (res)=>{
-      if(res.solely_code==100000){  
-        api.showToast('已添加备注','none')
-      }else{
-        api.showToast(res.msg,'none')
-      }
-      self.data.mainData = [];
-      self.data.sForm.title='';
-      self.setData({
-        web_sForm:self.data.sForm,
-      });
-       
-      if(wx.getStorageSync('threeInfo').user_type==2){
-        self.getMainData(true) 
-      };
+		    };
+		    api.orderUpdate(postData,callback);
+          }
+       },
+       fail: function (res) { },//接口调用失败的回调函数
+       complete: function (res) { },//接口调用结束的回调函数（调用成功、失败都会执行）
+    })
 
-    };
-    api.orderUpdate(postData,callback);
   },
 
 
@@ -233,8 +250,8 @@ showModel(e){
   const self = this;
   var id = api.getDataSet(e,'id');
     wx.showModal({
-       title: '确认提货',
-       content: '请确保此订单已提货',
+       title: '确认核销',
+       content: '是否核销此订单',
        showCancel: true,//是否显示取消按钮
        cancelText:"取消",//默认是“取消”
        cancelColor:'#ff0000',//取消文字的颜色
